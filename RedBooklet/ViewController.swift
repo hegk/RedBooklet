102a23cfad56e74877b03bfcf4dfe9f5880c2ec1
Reid <mission550w@gmail.com>
2023-08-31 20:32:10 +0800

    Initial Commit

---
+++ RedBooklet/ViewController.swift
@@ -0,0 +1,22 @@
+//
+// ViewController.swift
+// RedBooklet
+//
+// Created by Reid on 2023/8/31
+// Copyright © 2023 ___ORGANIZATIONNAME___. All rights reserved.
+
+/// 
+    
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
