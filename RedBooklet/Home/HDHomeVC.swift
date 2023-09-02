ae44fad28c0739c6827177571777e98029b07f22
Reid <mission550w@gmail.com>
2023-09-02 11:51:25 +0800

    add : home page tab setting

--- RedBooklet/Home/HDHomeVC.swift
+++ RedBooklet/Home/HDHomeVC.swift
@@ -13,7 +13,28 @@ import XLPagerTabStrip
 class HDHomeVC: ButtonBarPagerTabStripViewController {
 
     override func viewDidLoad() {
+        // MARK: ButtonBar 导选中条颜色
+        settings.style.selectedBarHeight = 2
+        settings.style.selectedBarBackgroundColor = UIColor(named: "theme")!
+        //        public var selectedBarVerticalAlignment: SelectedBarVerticalAlignment = .bottom
+        settings.style.selectedBarVerticalAlignment = .bottom
+        
+        // MARK: ButtonBar button设置
+        settings.style.buttonBarItemBackgroundColor = .clear
+        settings.style.buttonBarItemTitleColor = .label
+        settings.style.buttonBarItemFont = .systemFont(ofSize: 16)
+        settings.style.buttonBarItemLeftRightMargin = 0
+        
         super.viewDidLoad()
+        
+        containerView.bounces = false
+        changeCurrentIndexProgressive = { (oldCell: ButtonBarViewCell?, newCell: ButtonBarViewCell?, progressPercentage: CGFloat, changeCurrentIndex: Bool, animated: Bool) -> Void in
+            guard changeCurrentIndex == true else { return }
+
+            oldCell?.label.textColor = .secondaryLabel
+            newCell?.label.textColor = .label
+        }
+        
         
         
 
